1e9d072c85ff2e37e1fd0cbd14a1f96e31f90096
Lisa Doan <ldoan24@gmail.com>
2021-10-27 09:52:27 -0700

    Completed node101-express-server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,20 @@
 // import files and packages up here
-
+const express = require('express');
+const morgan = require('morgan');
+const data = require('./data.json')
 
 // create your express server below
-var app;
+var app = express();
 
 // add your routes and middleware below
+app.use(morgan('dev'));
+app.get("/", function (req, res) {
+  res.status(200).send("Hello world");
+});
+
+app.get("/data", function (req, res) {
+  res.status(200).send(data);
+});
 
 // finally export the express application
 module.exports = app;
